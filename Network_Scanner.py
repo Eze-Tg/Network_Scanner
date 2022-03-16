f5ed380b5869b5a19d72d7fbc92e6c6f0ad66603
friend <israelthankgod@gmail.com>
2022-03-16 22:08:33 +0100

    Initial commit

---
+++ Network_Scanner.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import scapy.all as scapy
+
+
+def scan(ip):
+    arp_request = scapy.ARP()
+    arp_request.pdst=ip
+    print(arp_request.summary())
+    scapy.ls(scapy.ARP())
+
+scan("10.0.2.1/24")
+
+
+
